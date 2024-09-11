9ed6b681d51744dbd033c8a6df51acb36d8ffe2f
cbsaffon <c.botero10@uniandes.edu.co>
2024-09-10 19:28:14 -0500

    Logo, dos columnas para analisis por dias y por horas

--- RentedBikeApp.py
+++ RentedBikeApp.py
@@ -18,12 +18,16 @@ ruta = "https://raw.githubusercontent.com/lbtriana/Proyecto_Bicicletas/main/Seou
 df = pd.read_csv(ruta)
 
 app.layout = html.Div([
-    html.H1("Rental Bikes Dashboard", style={'textAlign': 'center'}),
-    html.Img( src='Anexos_dash/20210708000693_0.jpg',
-        style={'display': 'block', 'margin-left': 'auto', 'margin-right': 'auto', 'width': '50%'}),
+    html.H1("- RENTAL BIKES DASHBOARD -", style={'fontFamily': 'Courier New' ,'color': 'green','textAlign': 'center', 'backgroundColor': '#ebf7e8'}),
+    html.Img( src='https://raw.githubusercontent.com/lbtriana/Proyecto_Bicicletas/main/Anexos_dash/20210708000693_0.jpg',
+        style={'display': 'block', 'margin-left': 'auto', 'margin-right': 'auto', 'width': '20%'}),
     html.Div([
 
         html.Div([
+            html.H3('DEMAND PREDICTION PER HOUR:', style={'fontFamily': 'Courier New' ,'color': 'green','textAlign': 'center', 'backgroundColor': '#ebf7e8'}),
+            html.H5('Please enter the values to predict demand:', style={'fontFamily': 'Helvetica' ,'color': '#585858','textAlign': 'left'}),
+            html.H6('(make sure the input makes sense, for example that the month and is_winter make sense)', style={'fontFamily': 'Helvetica' ,'color': '#7f807f','textAlign': 'left'}),
+            html.H6('(make sure input makes sense, for example that the month and is_winter dont contradict each other)', style={'fontFamily': 'Helvetica' ,'color': '#7f807f','textAlign': 'left'}),
             dcc.Dropdown(
                 
             ),
@@ -31,9 +35,13 @@ app.layout = html.Div([
                 
             )
         ],
-        style={'width': '48%', 'display': 'inline-block'}),
+        style={'width': '48%', 'display': 'inline-block', 'backgroundColor': '#3498db'}),
 
         html.Div([
+            html.H3('DEMAND PREDICTION PER DAY:', style={'fontFamily': 'Courier New' ,'color': 'green','textAlign': 'center', 'backgroundColor': '#ebf7e8'}),
+            html.H5('Please enter the values to predict demand:', style={'fontFamily': 'Helvetica' ,'color': '#585858','textAlign': 'left'}),
+            html.H6('(make sure the input makes sense, for example that the month and is_winter make sense)', style={'fontFamily': 'Helvetica' ,'color': '#7f807f','textAlign': 'left'}),
+            html.H6('(make sure input makes sense, for example that the month and is_winter dont contradict each other)', style={'fontFamily': 'Helvetica' ,'color': '#7f807f','textAlign': 'left'}),
             dcc.Dropdown(
                 
             ),
